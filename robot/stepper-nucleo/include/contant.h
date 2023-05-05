46c5392573153271d54b00e90e9f8bcbf28ba362
arc4t <arthur.hauswald@gmail.com>
2023-05-05 11:47:56 +0200

    Created ReadCommandFromSerial func

--- robot/stepper-nucleo/include/contant.h
+++ robot/stepper-nucleo/include/contant.h
@@ -15,7 +15,7 @@
 struct Position {
     float x;
     float y;
-    float theta;
+    float theta; // angle in radian
 };
 
 struct Distance {
--- robot/stepper-nucleo/src/main.cpp
+++ robot/stepper-nucleo/src/main.cpp
@@ -9,13 +9,33 @@ Odometry odom = Odometry(&initialPosition);
 Motor motor = Motor();
 Control control = Control(&odom, &motor);
 
-
 void setup() {
     Serial.begin(9600);
     pinMode(stepPinLeft, OUTPUT);
     pinMode(stepPinRight, OUTPUT);
     pinMode(dirPinLeft, OUTPUT);
     pinMode(dirPinRight, OUTPUT);
+}
+
+void ReadSerial() {
+    if (Serial.available() > 0) {
+        String cmd = Serial.readStringUntil('\n'); // read the incoming command
+        char *tok = strtok(&cmd[0], ":"); // parse the command using strtok()
+
+        int i = 0;
+        int cmdArguments[3] = {0, 0, 0};
+
+        while (tok != NULL && i < 3) {
+        cmdArguments[i] = atoi(tok); // convert the string token to integer
+        tok = strtok(NULL, ":"); // move to the next token
+        i++;
+        }
+
+        if (i == 3) {
+            Position target = {(float)cmdArguments[0],(float)cmdArguments[1], (float)cmdArguments[2]}; // x y theta
+            control.go_to(&target);
+        }
+    }
 }
 
 void loop() {
